46a753149da3467f63ec9f49244d2078456742d8
Yvan Razafindramanana <mail@ludeo.net>
2021-09-24 07:41:20 +0200

    Keep few more images in cache

--- api/src/Model/Cache/CachedImage.cs
+++ api/src/Model/Cache/CachedImage.cs
@@ -1,4 +1,19 @@
-using System;
+/*
+   Copyright 2021 Yvan Razafindramanana
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 using Microsoft.Azure.Cosmos.Table;
 
 namespace Ludeo.BingWallpaper.Model.Cache
@@ -6,7 +21,7 @@ namespace Ludeo.BingWallpaper.Model.Cache
     public class CachedImage : TableEntity
     {
         internal static string DefaultPartitionKey = "cache";
-        internal static int NumberOfEntriesToKeep = 10;
+        internal static int NumberOfEntriesToKeep = 50;
 
         public string? Uri { get; set; }
         public string? Title { get; set; }
