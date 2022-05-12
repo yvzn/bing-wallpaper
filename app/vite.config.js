f2faad936443efe82c783580b094396f804b1eeb
Yvan Razafindramanana <mail@ludeo.net>
2022-05-12 22:01:39 +0200

    Fix API URL for legacy/noscript browsers

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig, loadEnv } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()]
-})
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+	return {
+		plugins: [
+			reactRefresh(),
+			createHtmlPlugin({
+				minify: true,
+				inject: {
+					data: {
+						apiRedirectionLatestUrl:
+							env.VITE_API_REDIRECTION_LATEST_URL,
+					},
+				},
+			}),
+		],
+	};
+});
