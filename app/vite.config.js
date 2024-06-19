09140fd4467172f517ef8f7b29cea64e468014f8
Yvan Razafindramanana <mail@ludeo.net>
2024-06-19 16:30:18 +0000

    feat(app): remove obsolete vite-plugin-dotenv-in-html

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
-import dotEnvHTMLPlugin from "vite-plugin-dotenv-in-html";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
 	return {
 		plugins: [
 			reactRefresh(),
-			dotEnvHTMLPlugin(mode),
 		],
 	};
 });
